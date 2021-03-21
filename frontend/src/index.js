f0937c942cad72ea6e8f43cf7feaff48ce5558f1
KelvinUTS <49703204+KelvinUTS@users.noreply.github.com>
2021-03-22 02:17:22 +1100

    Update index.js
    fixing issues with login template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,9 +15,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
 
 
-/*
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -135,4 +135,3 @@ export default function SignIn() {
     </Container>
   );
 }
-*/
