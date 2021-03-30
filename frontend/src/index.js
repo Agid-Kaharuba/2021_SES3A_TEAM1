c2b9728541db9d84105515b55f2141daf33445d5
KelvinUTS <49703204+KelvinUTS@users.noreply.github.com>
2021-03-30 12:12:11 +1100

    Login UI finished
    Login page UI finished, not linked to backend yet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import SignIn from './signin'
 import LogIn from './login'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <SignIn/>
+    <LogIn/>
 
   </React.StrictMode>,
   document.getElementById('root')
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-//import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
-//import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 //creating the react hook
 const useStyles = makeStyles((theme) => ({
     //button setup
     submit: {
-        margin: theme.spacing(4, 0, 3), //top space, LR align, bottom space
+        margin: theme.spacing(4, 0, 1), //top space, LR align, bottom space
     },
     //form setup
     form: {
@@ -37,8 +31,8 @@ export default function LogIn() {
     const classes = useStyles(); //using hook to create classes object
     
     return (
-        <Container component = "main" maxWidth = "xs"> //set container size
-            <CssBaseline/> //reset styles to a consistent baseline
+        <Container component = "main" maxWidth = "xs"> {/*set container size*/}
+            <CssBaseline/> {/*reset styles to a consistent baseline*/}
             <div className = {classes.paper}>
                 <Typography variant = "h3">LOGIN</Typography>
 
@@ -85,11 +79,9 @@ export default function LogIn() {
                 >
                 Sign Up
                 </Button>
-
-
                 </form>
-            </div>
 
+            </div>
         </Container>
     );
 }
