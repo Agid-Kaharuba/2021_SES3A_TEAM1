0367824f24ae2a0aff142647fecabd0ae7633274
mitcmurph <mitcmurph@github.com>
2021-03-31 22:35:31 +1100

    mongodb

--- expressJS-typescript/src/app.ts
+++ expressJS-typescript/src/app.ts
@@ -1,14 +1,29 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+// import morgan from "morgan";
+import courseRouter from "./routes/course-router"
+
+
+dotenv.config();
+
+const mongo_uri = process.env.DATABASE_URL as string;
+mongoose.connect(mongo_uri, { useNewUrlParser: true }, function(err) {
+	if (err) {
+		throw err;
+	} else {
+		console.log(`Successfully connected to ${mongo_uri}`);
+	}
+});
 
 const app: Application = express();
 
-const add = (a: number, b: number): number => {
-    return a + b;
-}
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    console.log(add(5,5));
-    res.send('Hello')
+// Routes
+app.use("/course", courseRouter);
+
+const server = app.listen(process.env.API_PORT, () => {
+    console.log(`Server is now running at:  http://localhost:${process.env.API_PORT}`);
 });
-
-app.listen(5000, () => console.log("Server running on 5000"));
