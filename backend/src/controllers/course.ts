b175875c88530da91b22031261f70b0e4a1a7888
mitcmurph <mitcmurph@gmail.com>
2021-04-07 01:04:16 -0700

    get and create endpoint

--- backend/src/controllers/course.ts
+++ backend/src/controllers/course.ts
@@ -6,4 +6,21 @@ export default class CourseController {
         const courses = await Course.find();
         res.json(courses)
     }
+
+    public async get(req: Request, res: Response) {
+        const course = await Course.findOne({
+			_id: req.params.courseId
+		});
+        res.json(course)
+    }
+
+    public async create(req: Request, res: Response) {
+		const body = req.body;
+		const newCourseRequest = new Course({
+			name: body.name,
+			description: body.description
+		} as any);
+		newCourseRequest.save();
+        res.json(newCourseRequest);
+    }
 }
--- backend/src/routes/course-router.ts
+++ backend/src/routes/course-router.ts
@@ -2,5 +2,7 @@ import { Express } from "express";
 import CourseController from "../controllers/course";
 
 export const CourseRoute = (app: Express, controller: CourseController) => {
-    app.get("/course", controller.getAll)
+    app.get("/course", controller.getAll);
+    app.get("/course/:courseId", controller.get);
+    app.post("/course", controller.create);
 }
