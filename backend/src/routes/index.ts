2a1c4880d1478870bd82127405a61bac717be1a1
13204767 <13204767@student.uts.edu.au>
2021-04-09 09:31:51 +1000

    Get all recipes working

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import ApiInitializer from "../initializer";
 import { CourseRoute } from "./course-router";
+import { RecipeRoute } from "./recipe-router";
 import CourseController from "../controllers/course";
+import RecipeController from "../controllers/recipe";
 
 
 export const Routes = (init: ApiInitializer) => {
     CourseRoute(init.getApp(), new CourseController());
+    RecipeRoute(init.getApp(), new RecipeController());
 }
