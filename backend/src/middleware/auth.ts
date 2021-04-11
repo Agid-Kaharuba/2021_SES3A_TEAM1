7ba8d0f002cd2cbbe2b07bad8738d4b250c97b48
mitcmurph <mitcmurph@gmail.com>
2021-04-11 07:46:42 -0700

    auth middleware

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,47 @@
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
+import User from "../model/user"
+import dotenv from "dotenv";
+import ResponseService from "../helpers/response"
+
+dotenv.config();
+
+export const checkToken = async (req: Request, res: Response, next: any) => {
+    await verifyUser(req, res, next);
+};
+
+const getAuthToken = (req: Request): string|null => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ")[0] === "Bearer"
+    ) {
+        return req.headers.authorization.split(" ")[1];
+    } else {
+		return null;
+    }
+};
+
+const getUser = async (token: any) => {
+	try {
+		const payload = jwt.verify(token, `${process.env.TOKEN_SECRET}`);
+		// @ts-ignore
+		return await User.findOne({ username: payload.username });
+	} catch (err) {
+		return undefined;
+	}
+};
+
+const verifyUser = async (req: Request,	res: Response,	next: any) => {
+    const token = getAuthToken(req);
+    console.log(token);
+	if (!token) {
+        ResponseService.unauthorizedResponse(res, "Unauthorized - no token found");
+	} else {
+		const user = await getUser(token);
+		if (user !== undefined) {
+			next();
+		} else {
+            ResponseService.unauthorizedResponse(res);
+		}
+	}
+};
