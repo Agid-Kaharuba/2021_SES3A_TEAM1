d2564d83a12856f4ce19c6a2389ecd2e59d0d3a5
mitcmurph <mitcmurph@gmail.com>
2021-04-11 06:22:36 -0700

    unit tests for courses

--- backend/src/tests/app.unit.ts
+++ backend/src/tests/app.unit.ts
@@ -4,8 +4,13 @@ process.env.DATABASE_NAME = 'unit-test'
 import express from 'express';
 import request from "supertest";
 import { expect } from "chai";
+import mongoose from "mongoose";
+import Course from "../model/course";
+import Recipe from "../model/recipe";
+import Step from "../model/step";
+import User from "../model/user";
 
-describe("GET /", () => {
+describe("Prepare Unit Test", () => {
   var server: any;
 
   beforeEach(() => {
@@ -18,7 +23,16 @@ describe("GET /", () => {
     server.close();
   });
 
-  it(" should be online", function (done) {
-    request(server).get("/").expect(200, done);
+  it("should empty Course", async function () {
+    await Course.deleteMany({});
+  });
+  it("should empty Recipe", async function () {
+    await Recipe.deleteMany({});
+  });
+  it("should empty Step", async function () {
+    await Step.deleteMany({});
+  });
+  it("should empty User", async function () {
+    await User.deleteMany({});
   });
 });
