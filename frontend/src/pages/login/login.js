f06c9112d45026b0e80125167e44989d6682e706
mitcmurph <mitcmurph@gmail.com>
2021-04-17 20:55:00 -0700

    indent

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -12,85 +12,85 @@ import { useHistory } from 'react-router-dom';
 
 //creating the react hook
 const useStyles = makeStyles((theme) => ({
-    //button setup
-    submit: {
-        margin: theme.spacing(4, 0, 1), //top space, LR align, bottom space
-    },
-    //form setup
-    form: {
-        marginTop: theme.spacing(2), //top space
-        width: '100%', //form width
-    },
-    //setup for the overall page
-    paper: {
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        marginTop: theme.spacing(7),
-    },
+  //button setup
+  submit: {
+    margin: theme.spacing(4, 0, 1), //top space, LR align, bottom space
+  },
+  //form setup
+  form: {
+    marginTop: theme.spacing(2), //top space
+    width: '100%', //form width
+  },
+  //setup for the overall page
+  paper: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(7),
+  },
 }));
 
 export default function LogIn() {
-    const classes = useStyles(); //using hook to create classes object
-    let history = useHistory();
+  const classes = useStyles(); //using hook to create classes object
+  let history = useHistory();
 
-    const signupClick = () => {
-        history.push('/signup')
-    }
-    
-    return (
-        <Container component = "main" maxWidth = "xs"> {/*set container size*/}
-            <CssBaseline/> {/*reset styles to a consistent baseline*/}
-            <div className = {classes.paper}>
-                <Typography variant = "h3">LOGIN</Typography>
+  const signupClick = () => {
+    history.push('/signup')
+  }
 
-                <form className = {classes.form} noValidate>
-                <TextField
-                    label="Username"
-                    variant="outlined"
-                    margin="normal"
-                    name="username"
-                    id="username"
-                    required //must have input to submit the form
-                    fullWidth
-                />
-                <Link href="#" variant="body2">Forgot username?</Link>
-                
-                <TextField
-                    label="Password"
-                    variant="outlined"
-                    margin="normal"
-                    name="password"
-                    id="password"
-                    type="password" //hide password entered
-                    required
-                    fullWidth
-                />
-                <Link href="#" variant="body2">Forgot password?</Link>
+  return (
+    <Container component="main" maxWidth="xs"> {/*set container size*/}
+      <CssBaseline /> {/*reset styles to a consistent baseline*/}
+      <div className={classes.paper}>
+        <Typography variant="h3">LOGIN</Typography>
 
-                <Button
-                    type="submit"
-                    className={classes.submit}
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                >
-                Login
-                </Button>
+        <form className={classes.form} noValidate>
+          <TextField
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            name="username"
+            id="username"
+            required //must have input to submit the form
+            fullWidth
+          />
+          <Link href="#" variant="body2">Forgot username?</Link>
 
-                <Button
-                    type="submit"
-                    className={classes.submit}
-                    variant="contained"
-                    color="primary"
-                    fullWidth
-                    onClick={signupClick}
-                >
-                Sign Up
-                </Button>
-                </form>
+          <TextField
+            label="Password"
+            variant="outlined"
+            margin="normal"
+            name="password"
+            id="password"
+            type="password" //hide password entered
+            required
+            fullWidth
+          />
+          <Link href="#" variant="body2">Forgot password?</Link>
 
-            </div>
-        </Container>
-    );
+          <Button
+            type="submit"
+            className={classes.submit}
+            variant="contained"
+            color="primary"
+            fullWidth
+          >
+            Login
+          </Button>
+
+          <Button
+            type="submit"
+            className={classes.submit}
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={signupClick}
+          >
+            Sign Up
+          </Button>
+        </form>
+
+      </div>
+    </Container>
+  );
 }
