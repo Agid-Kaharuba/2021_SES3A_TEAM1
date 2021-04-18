acd20a15d349967465ff03f838728e7078804759
mitcmurph <mitcmurph@gmail.com>
2021-04-18 06:56:41 -0700

    connect signup to backend

--- frontend/src/pages/signup/signup.js
+++ frontend/src/pages/signup/signup.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // IMPORT COMPONENTS
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,6 +12,9 @@ import { FormControl } from '@material-ui/core';
 import { RadioGroup } from '@material-ui/core';
 import { FormControlLabel } from '@material-ui/core';
 import { Radio } from '@material-ui/core';
+
+import api from "../../helpers/api";
+import { AuthContext } from "../../context/auth";
 
 //creating the react hook
 const useStyles = makeStyles((theme) => ({
@@ -34,14 +38,71 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
     const classes = useStyles(); //using hook to create classes object
+    let history = useHistory();
+    const [formState, setFormState] = useState({});
+    const [resultState, setResultState] = React.useState(undefined);
+    const { authState, setAuthState } = React.useContext(AuthContext);
     
+
+  const buildResult = () => {
+    return (
+      <>
+        <p>{resultState}</p>
+      </>
+    );
+  };
+    
+  const handleChange = async (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    setFormState({
+      ...formState, [name]: value,
+    });
+  }
+
+  const handleSignup = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    if (formState.password !== formState.confirmPassword){
+        setResultState("Passwords do not match")
+    }
+    else {        
+        var res;
+        try {
+            res = (await api.auth.register(formState));
+
+            res = (await api.auth.login(formState));
+            setAuthState({
+                authenticated: true,
+                ...res.data,
+            })
+            history.push('/dashboard')
+        }
+        catch (err) {
+            setResultState(err.response.data.err);
+        }
+    }
+  }
+
     return (
         <Container component = "main" maxWidth = "xs"> {/*set container size*/}
             <CssBaseline/> {/*reset styles to a consistent baseline*/}
             <div className = {classes.paper}>
                 <Typography variant = "h3">SIGN UP</Typography>
 
-                <form className = {classes.form} noValidate>
+                <form className = {classes.form} onSubmit={handleSignup}>
+                <TextField
+                    label="Staff ID"
+                    variant="outlined"
+                    margin="normal"
+                    name="staffid"
+                    id="staffid"
+                    required //must have input to submit the form
+                    fullWidth
+                    onChange={handleChange}
+                />
                 <TextField
                     label="Username"
                     variant="outlined"
@@ -50,6 +111,42 @@ export default function SignUp() {
                     id="username"
                     required //must have input to submit the form
                     fullWidth
+                    onChange={handleChange}
+                />
+
+                <TextField
+                    label="First Name"
+                    variant="outlined"
+                    margin="normal"
+                    name="firstname"
+                    id="First Name"
+                    required //must have input to submit the form
+                    fullWidth
+                    onChange={handleChange}
+                />
+
+                <TextField
+                    label="Last Name"
+                    variant="outlined"
+                    margin="normal"
+                    name="lastname"
+                    id="Last Name"
+                    required //must have input to submit the form
+                    fullWidth
+                    onChange={handleChange}
+                />
+
+                <TextField
+                    label="Email"
+                    variant="outlined"
+                    margin="normal"
+                    name="email"
+                    id="email"
+                    type="email"
+                    autoComplete="email"
+                    required //must have input to submit the form
+                    fullWidth
+                    onChange={handleChange}
                 />
                 
                 <TextField
@@ -61,60 +158,31 @@ export default function SignUp() {
                     type="password" //hide password entered
                     required
                     fullWidth
+                    onChange={handleChange}
                 />
 
                 <TextField
                     label="Confirm Password"
                     variant="outlined"
                     margin="normal"
-                    name="confirm password"
+                    name="confirmPassword"
                     id="confirm password"
                     type="password" //hide password entered
                     required
                     fullWidth
-                />
-
-                <TextField
-                    label="First Name"
-                    variant="outlined"
-                    margin="normal"
-                    name="First Name"
-                    id="First Name"
-                    required //must have input to submit the form
-                    fullWidth
-                />
-
-                <TextField
-                    label="Last Name"
-                    variant="outlined"
-                    margin="normal"
-                    name="Last Name"
-                    id="Last Name"
-                    required //must have input to submit the form
-                    fullWidth
-                />
-
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    margin="normal"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
-                    required //must have input to submit the form
-                    fullWidth
+                    onChange={handleChange}
                 />
 
                 <FormControl component="fieldset">
                 {/* value={value} onChange={handleChange} --event handling, goes in RadioGroup below-- */}
-                <RadioGroup aria-label="account type" name="account type" row>
+                <RadioGroup aria-label="account type" name="isSupervisor" row onChange={handleChange}>
                     <FormControlLabel 
-                        value="supervisor" 
+                        value="true"
                         control={<Radio required={true}/>}
                         label="Supervisor"
                     />
                     <FormControlLabel 
-                        value="employee" 
+                        value="false"
                         control={<Radio required={true}/>}
                         label="Employee"
                     />
@@ -134,6 +202,7 @@ export default function SignUp() {
                 </form>
 
             </div>
+            {resultState !== undefined && buildResult()}
         </Container>
     );
 }
