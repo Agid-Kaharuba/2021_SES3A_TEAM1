74bd9d23c47e648e54c65a38c158fd731cea54e9
mitcmurph <mitcmurph@gmail.com>
2021-04-17 22:10:52 -0700

    verify auth endpoint for navigation mangement

--- frontend/src/helpers/api/index.js
+++ frontend/src/helpers/api/index.js
@@ -1,13 +1,15 @@
 import {
+  verify,
   registerUser,
-  verifyLogin
+  loginUser
 } from "./auth";
 
 export const API_HOST = "http://localhost:4000";
 
 export default {
     auth: {
-      login: verifyLogin,
+      verify: verify,
+      login: loginUser,
       register: registerUser
     }
 }
