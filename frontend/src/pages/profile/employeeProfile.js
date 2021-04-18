a24b97cecf50e4c0e50aa05c632a7b54bbba5e30
mitcmurph <mitcmurph@gmail.com>
2021-04-18 00:12:43 -0700

    indent

--- frontend/src/pages/profile/employeeProfile.js
+++ frontend/src/pages/profile/employeeProfile.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
-import {Button, TextField, Container, Typography, Avatar} from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, TextField, Container, Typography, Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-import UploadImageForm from "../../components/upload"; 
+import UploadImageForm from "../../components/upload";
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -10,14 +10,14 @@ const useStyles = makeStyles(theme => ({
   },
   textField: {
     marginTop: theme.spacing(0.5),
-		marginBottom: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
   },
   typography: {
     marginTop: theme.spacing(0.5),
-		marginBottom: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
   },
   heading: {
-    marginTop:theme.spacing(3),
+    marginTop: theme.spacing(3),
     marginBottom: theme.spacing(0.5)
   },
   image: {
@@ -39,170 +39,170 @@ const useStyles = makeStyles(theme => ({
 
 export default function EmployeeProfile(props) {
 
-    let userData=[props.ProfileState]
-    const classes = useStyles(); 
+  let userData = [props.ProfileState]
+  const classes = useStyles();
 
-    const [employee, setEmployeeDetails] = useState({
-        fName: "",
-        lName: "",
-        email: "",
-        password:"",
-        staffID:""
-      });
+  const [employee, setEmployeeDetails] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    password: "",
+    staffID: ""
+  });
 
-    // const pImage = useState({
-    //   profileImg:'https://cdn3.iconfinder.com/data/icons/gradient-general-pack/512/user-01-512.png'
-    // });
+  const pImage = useState({
+    profileImg:'https://cdn3.iconfinder.com/data/icons/gradient-general-pack/512/user-01-512.png'
+  });
 
-    // const {profileImg} = this.pImage;
+  // const {profileImg} = this.pImage;
 
-    function handleChange(e) {
-        console.log(e.target);
-         const{name,value} = e.target;
+  function handleChange(e) {
+    console.log(e.target);
+    const { name, value } = e.target;
 
-         setEmployeeDetails(prevValue => {
-             if(name==="fName"){
-                 return {
-                    fName: value,
-                    lName: prevValue.lName,
-                    email: prevValue.email,
-                    password: prevValue.password,
-                    staffID: prevValue.staffID
-                 };
-             } else if (name==="lName"){
-                 return {
-                     fName: prevValue.fName,
-                     lName: value,
-                     email: prevValue.email,
-                     password: prevValue.password,
-                     staffID: prevValue.staffID
-                 };
-             } else if (name==="email"){
-                 return {
-                    fName: prevValue.fName,
-                    lName: prevValue.lName,
-                    email: value,
-                    password: prevValue.password,
-                    staffID: prevValue.staffID
-                 };
-                } else if (name==="password"){
-                  return {
-                     fName: prevValue.fName,
-                     lName: prevValue.lName,
-                     email: prevValue.email,
-                     password: value,
-                     staffID: prevValue.staffID
-                  };
-             } else if (name==="staffID"){
-                 return {
-                    fName: prevValue.fName,
-                    lName: prevValue.lName,
-                    email: prevValue.email,
-                    password: prevValue.password,
-                    staffID: value
-                 };
-             }
-         });
-    }
-
-    function saveChanges() {
-
-    }
-    
-    const uploadedImage = React.useRef(null);
-    const imageUploader = React.useRef(null);
-
-    const handleImageUpload = e => {
-      const [file] = e.target.files;
-      if(file){
-        const reader = new FileReader();
-        const {current} = uploadedImage;
-        current.file = file;
-        reader.onload = (e) => {
-          current.src = e.target.result;
-        }
-        reader.readAsDataURL(file);
+    setEmployeeDetails(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email,
+          password: prevValue.password,
+          staffID: prevValue.staffID
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email,
+          password: prevValue.password,
+          staffID: prevValue.staffID
+        };
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value,
+          password: prevValue.password,
+          staffID: prevValue.staffID
+        };
+      } else if (name === "password") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: prevValue.email,
+          password: value,
+          staffID: prevValue.staffID
+        };
+      } else if (name === "staffID") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: prevValue.email,
+          password: prevValue.password,
+          staffID: value
+        };
       }
-    };
+    });
+  }
 
-    return (
-      <Container maxWidth="sm">
-        <div className={classes.container}>
-       <Typography variant="h4" className={classes.heading}>PROFILE</Typography>
-        
+  function saveChanges() {
+
+  }
+
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = e => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = (e) => {
+        current.src = e.target.result;
+      }
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <div className={classes.container}>
+        <Typography variant="h4" className={classes.heading}>PROFILE</Typography>
+
         {/*Accepting only files with image type*/}
-        <input type = "file" id="input" accept="image/*" onChange={handleImageUpload} ref={imageUploader} className={classes.imageUpload}/>  
+        <input type="file" id="input" accept="image/*" onChange={handleImageUpload} ref={imageUploader} className={classes.imageUpload} />
         <div>
           <img className={classes.image} ref={uploadedImage} />
-          <br/>
-          <Button size="small" onClick={() => imageUploader.current.click()}variant="outlined" color="secondary">Upload Image</Button>
+          <br />
+          <Button size="small" onClick={() => imageUploader.current.click()} variant="outlined" color="secondary">Upload Image</Button>
         </div>
-        </div>
+      </div>
 
-        <br/>
-        <Typography variant="subtitle1" className={classes.typography}>First Name</Typography>
-        <TextField
-          id="outlined-required"
-          name = "fName"
-          onChange={handleChange}
-          value={employee.fName}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-          // label="First Name"
-        />
-        <br/>
-        <Typography variant="subtitle1" className={classes.typography}>Last Name</Typography>
-        <TextField
-          id="outlined-required"
-          name = "lName"
-          onChange={handleChange}
-          value={employee.lName}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        />
-        <br/>
-        <Typography variant="subtitle1" className={classes.typography}>Email</Typography>
-        <TextField
-          id="outlined-required"
-          name = "email"
-          onChange={handleChange}
-          value={employee.email}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        />
-        <br/>
-        <Typography variant="subtitle1" className={classes.typography}>Password</Typography>
-        <TextField
-          id="outlined-password-input"
-          type="password"
-          name = "password"
-          autoComplete="current-password"
-          onChange={handleChange}
-          value={employee.password}
-          variant="outlined"
-          fullWidth
-          className={classes.password}
-        />
-        <br/>
-        <Typography variant="subtitle1" className={classes.typography}>Staff ID</Typography>
-        <TextField
-          id="outlined-required"
-          name = "staffID"
-          onChange={handleChange}
-          value={employee.staffID}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        />
-        <br/>
-        <div className={classes.container}>
-        <Button onClick= {saveChanges} variant="contained" color="secondary" className={classes.button}>
+      <br />
+      <Typography variant="subtitle1" className={classes.typography}>First Name</Typography>
+      <TextField
+        id="outlined-required"
+        name="fName"
+        onChange={handleChange}
+        value={employee.fName}
+        variant="outlined"
+        fullWidth
+        className={classes.textField}
+      // label="First Name"
+      />
+      <br />
+      <Typography variant="subtitle1" className={classes.typography}>Last Name</Typography>
+      <TextField
+        id="outlined-required"
+        name="lName"
+        onChange={handleChange}
+        value={employee.lName}
+        variant="outlined"
+        fullWidth
+        className={classes.textField}
+      />
+      <br />
+      <Typography variant="subtitle1" className={classes.typography}>Email</Typography>
+      <TextField
+        id="outlined-required"
+        name="email"
+        onChange={handleChange}
+        value={employee.email}
+        variant="outlined"
+        fullWidth
+        className={classes.textField}
+      />
+      <br />
+      <Typography variant="subtitle1" className={classes.typography}>Password</Typography>
+      <TextField
+        id="outlined-password-input"
+        type="password"
+        name="password"
+        autoComplete="current-password"
+        onChange={handleChange}
+        value={employee.password}
+        variant="outlined"
+        fullWidth
+        className={classes.password}
+      />
+      <br />
+      <Typography variant="subtitle1" className={classes.typography}>Staff ID</Typography>
+      <TextField
+        id="outlined-required"
+        name="staffID"
+        onChange={handleChange}
+        value={employee.staffID}
+        variant="outlined"
+        fullWidth
+        className={classes.textField}
+      />
+      <br />
+      <div className={classes.container}>
+        <Button onClick={saveChanges} variant="contained" color="secondary" className={classes.button}>
           Save
         </Button>
-        </div>
-      </Container>
-    );
+      </div>
+    </Container>
+  );
 }
