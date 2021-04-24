7e51d7a751ce83ef364c006da115d36f5feb141f
Agid-Kaharuba <razinagida@gmail.com>
2021-04-25 01:14:02 +1000

    Add XR auto attaching canvas and auto scaling collider buttons

---
+++ unity/XRTProject/Assets/Scripts/UI/UIBoxColliderAutoScaler.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UIBoxColliderAutoScaler : MonoBehaviour
+{
+    [SerializeField] private BoxCollider collider;
+    [SerializeField] private float depth = 0.06f;
+
+    private RectTransform rectTransform;
+
+    private void Update()
+    {
+        if (rectTransform == null)
+            rectTransform = GetComponent<RectTransform>();
+            
+        Vector3 colliderSize = collider.size;
+        colliderSize.x = rectTransform.rect.width / collider.transform.localScale.x;
+        colliderSize.y = rectTransform.rect.height / collider.transform.localScale.y;
+        colliderSize.z = depth;
+        collider.size = colliderSize;
+    }
+}
