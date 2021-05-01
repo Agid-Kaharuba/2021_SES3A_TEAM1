a53d7c8a47cbb0d2ec4be0abaa27270947be3246
herrickfeng <herrcikfeng@gmail.com>
2021-05-01 18:03:48 +1000

    Added Taskslist

--- frontend/src/components/Chart/index.js
+++ frontend/src/components/Chart/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Bar, Line, Pie} from 'react-chartjs-2';
 
 class Chart extends Component{
-    constructor(){
+    constructor(props){
         super(props);
         this.state = {
             chartData:{
