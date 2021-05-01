81327da02e31210175d2ad29729f8c6b21225ada
herrickfeng <herrcikfeng@gmail.com>
2021-05-01 17:50:00 +1000

    change in statistics.js

--- frontend/src/pages/statistics/Statistics.js
+++ frontend/src/pages/statistics/Statistics.js
@@ -18,16 +18,6 @@ const useStyles = makeStyles({
     },
   })
 
-  function createData(name, description, duration, view) {
-    return {name,description, duration, view};
-  }
-
-  //0 is a placeholder for the view button
-  const rows = [
-      createData('Task 1', 'Learn the essentials of CPR through an interactive simulation', 10,0),
-      createData('Task 2', 'Learn how to mitigate safety hazards in the workplace', 20,0),
-      createData('Task 3', 'Learn the safety terminology', 5,0)
-  ];
 
 export default function Statistics() {
     const classes = useStyles();
