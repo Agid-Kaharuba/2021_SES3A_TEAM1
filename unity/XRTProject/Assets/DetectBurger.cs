f90b2a509ea16c5bf7b4b1ed9e0c2126e1295273
Andy <andyau99@gmail.com>
2021-05-02 18:06:58 +1000

    Finished tray object detection
    Added Tags for Plates and sample deletion/debug

---
+++ unity/XRTProject/Assets/DetectBurger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectBurger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Plates"))
+        {
+            Debug.Log("Collision Detected");
+            // For some reason this is crashing the app but it works
+            Destroy(collider.gameObject);
+        }
+    }
+}
