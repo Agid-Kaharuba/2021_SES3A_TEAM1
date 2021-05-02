4d48cad012ba54f3f5858c46cd7b32e5f2ca33e8
Agid-Kaharuba <razinagida@gmail.com>
2021-05-03 02:09:15 +1000

    Add few comments about TrainingModule fetching

--- unity/XRTProject/Assets/Scripts/TrainingManager.cs
+++ unity/XRTProject/Assets/Scripts/TrainingManager.cs
@@ -70,7 +70,10 @@ public class TrainingManager : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(apiService.GetTrainingModule("608eae5ab7dd3233a46916f7", (response) =>
+        // TODO fetch this from the command line when launching from web to unity
+        string trainingModuleId = "608eae5ab7dd3233a46916f7";
+        
+        StartCoroutine(apiService.GetTrainingModule(trainingModuleId, (response) =>
         {
             if (response is BackendErrorResponse errorReponse)
             {
