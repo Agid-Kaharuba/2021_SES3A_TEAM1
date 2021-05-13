17e5f686c028d17221e152939ef003c742e21590
13204767 <13204767@student.uts.edu.au>
2021-05-13 22:58:06 +1000

    - Employees can't edit recipe anymore

--- frontend/src/pages/viewRecipe/viewRecipe.js
+++ frontend/src/pages/viewRecipe/viewRecipe.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { Button, Typography, Divider, Box, FormControl, Select, MenuItem, Grid, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { AuthContext } from "../../context/auth";
 import api from "../../helpers/api";
+import ViewRecipeSup from "../../components/viewRecipes/viewRecipesSup";
+import ViewRecipeEmp from "../../components/viewRecipes/viewRecipesEmp";
 
 const useStyles = makeStyles({
     bold: {
@@ -59,61 +61,20 @@ export default function ViewRecipe(props) {
         return (<h1>LOADING</h1>)
     }
     else {
-        return (
-            <>
-            <Box m={5}>
-                <Grid container spacing={2} justify="space-between">
-                    <Grid item>
-                        <Typography className={classes.bold} variant='h4'>
-                            Recipe
-                   </Typography>
-                    </Grid>
-                    <Grid item align="right">
-                    {authState.user.isSupervisor && (
-                        <Button variant="contained" style={{ width: 80 }} color={editState ? "secondary" : "primary"} size="large" onClick={handleEdit}>
-                            {editState ? "Edit" : "Save"}
-                        </Button>)}
-                    </Grid>
-                </Grid>
-                <Divider variant="middle" />
-            </Box>
-            <Box m={5}>
-
-                <Grid container spacing={2} direction="column" justify="space-between">
-                    <Grid>
-                        <Typography className={classes.bold} variant='h6'>
-                            Name
-            </Typography>
-                        <TextField value={recipeState.name} id="name" disabled={editState} variant="outlined" fullWidth margin='normal' onChange={handleChange} />
-                    </Grid>
-                    <Grid>
-                        <Typography className={classes.bold} variant='h6'>
-                            Category
-            </Typography>
-                        <TextField value={recipeState.category} id="category" disabled={editState} variant="outlined" fullWidth margin='normal' onChange={handleChange} />
-                    </Grid>
-                    <Grid>
-                        <Typography className={classes.bold} variant='h6'>
-                            Ingredients
-            </Typography>
-                        <TextField value={recipeState.ingredients} id="ingredients" disabled={editState} variant="outlined" fullWidth margin='normal' multiline rows={10} onChange={handleChange} />
-                    </Grid>
-                    <Grid>
-                        <Typography className={classes.bold} variant='h6'>
-                            Steps
-            </Typography>
-                        <TextField value={recipeState.steps} id="steps" disabled={editState} variant="outlined" fullWidth margin='normal' multiline rows={10} onChange={handleChange} />
-                    </Grid>
-                    <Grid container
-                        direction="row"
-                        justify="center"
-                        alignItems="center"
-                        margin="normal">
-                    </Grid>
-
-                </Grid>
-            </Box>
-        </>
-        )
+        if(authState.user.isSupervisor){
+        return(
+            <ViewRecipeSup 
+            handleEdit = {handleEdit}
+            editState = {editState} 
+            recipeState = {recipeState}  
+            handleChange = {handleChange}>
+            </ViewRecipeSup>
+        );
+        }
+        else{
+            return(
+                <ViewRecipeEmp recipeState = {recipeState}/>
+            )
+        }
     }
 }
