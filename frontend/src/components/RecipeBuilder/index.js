f14df5097ef4395a73ffaa00e818f2f378dacf03
mitcmurph <mitcmurph@gmail.com>
2021-05-14 23:39:22 +1000

    Add menu ingredients

--- frontend/src/components/RecipeBuilder/index.js
+++ frontend/src/components/RecipeBuilder/index.js
@@ -15,16 +15,36 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
+const insertIntoArray = (list, value, index) => {
+  list.splice(list, index, value);
+};
+
 export default function RecipeBuilder(props) {
-  const [state, setState] = useState([
+  const [state, setState] = useState([]);
+  var itemCount = 0;
+
+  const ingredients = [
     { id: "top-bun", value: "Top Bun", src: "https://www.metro.ca/userfiles/image/infographics/images/burgers/5-COWBOY/5-Cowboy-Bun-Top.png" },
     { id: "patty", value: "Patty", src: "https://image.shutterstock.com/image-photo/single-grilled-hamburger-patty-isolated-260nw-534672568.jpg" },
     { id: "bottom-bun", value: "Bottom Bun", src: "https://png.pngitem.com/pimgs/s/344-3449103_bun-bread-bottom-hd-png-download.png" }
-  ]);
+  ];
 
   const onDragEnd = (result) => {
-    var newOrder = reorder(state, result.source.index, result.destination.index)
-    setState(newOrder);
+    if (!result.destination) {
+      return;
+    }
+
+    if (result.source.droppableId == "ingredients" && result.destination.droppableId == "recipe") {
+      const ingr = { ...ingredients[result.source.index] };
+      ingr.id = "item-" + itemCount++
+      state.splice(result.destination.index, 0, ingr);
+      setState(state);
+    }
+    else if (result.source.droppableId == "recipe" && result.destination.droppableId == "recipe") {
+      var newOrder = reorder(state, result.source.index, result.destination.index)
+      setState(newOrder);
+    }
+    console.log(state);
   }
 
   const TaskList = () => {
@@ -33,27 +53,57 @@ export default function RecipeBuilder(props) {
 
   return (
     state && (
-      <Box mx={20}>
+      <Box mx={50}>
         <Paper style={{ backgroundColor: "white" }} elevation={3}>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="recipe">
-              {provided => (
-                <Grid
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  container
-                  direction="column"
-                  alignItems="center"
-                  justify="center"
-                >
-                  {state.map((ingredient, index) => <Ingredient ingredient={ingredient} index={index} />)}
-                  {provided.placeholder}
-                </Grid >
-              )}
-            </Droppable>
-          </DragDropContext>
+          <Grid container>
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Grid item style={{ flex: 0.3 }}>
+                <Droppable droppableId="ingredients">
+                  {provided => (
+                    <Grid
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      container
+                      direction="column"
+                      alignItems="center"
+                      justify="center"
+                    >
+                      {ingredients.map((ingredient, index) => <Ingredient ingredient={ingredient} index={index} />)}
+                      {provided.placeholder}
+                    </Grid >
+                  )}
+                </Droppable>
+              </Grid>
+
+              <Grid item style={{ flex: 0.7 }}>
+                <Droppable droppableId="recipe">
+                  {(provided, snapshot) => (
+                    <Paper>
+                      <Grid
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{ "min-height": "70vh" }}
+                        isDraggingOver={snapshot.isDraggingOver}
+                        container
+                        direction="column"
+                        alignItems="center"
+                      >
+                        {state && state.length > 0
+                          ? state.map((ingredient, index) => <Ingredient ingredient={ingredient} index={index} />)
+                          : <Typography variant='h4'>
+                              Drop ingredients here
+                            </Typography>
+                        }
+                        {provided.placeholder}
+                      </Grid>
+                    </Paper>
+                  )}
+                </Droppable>
+              </Grid>
+            </DragDropContext>
+          </Grid>
         </Paper>
-      </Box>
+      </Box >
     )
   );
 }
