1cfed726f568c9783341f76b43b5ffc09b72febd
mitcmurph <mitcmurph@gmail.com>
2021-05-14 22:40:05 +1000

    update state on new order

--- frontend/src/components/RecipeBuilder/index.js
+++ frontend/src/components/RecipeBuilder/index.js
@@ -7,6 +7,14 @@ import api from "../../helpers/api";
 import Recipe from "./recipe"
 import Ingredient from "./ingredient"
 
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
 export default function RecipeBuilder(props) {
   const [state, setState] = useState([
     { id: "top-bun", value: "Top Bun", src: "https://www.metro.ca/userfiles/image/infographics/images/burgers/5-COWBOY/5-Cowboy-Bun-Top.png" },
@@ -15,8 +23,8 @@ export default function RecipeBuilder(props) {
   ]);
 
   const onDragEnd = (result) => {
-
-    console.log(result)
+    var newOrder = reorder(state, result.source.index, result.destination.index)
+    setState(newOrder);
   }
 
   const TaskList = () => {
@@ -25,23 +33,27 @@ export default function RecipeBuilder(props) {
 
   return (
     state && (
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="recipe">
-          {provided => (
-            <Grid
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              container
-              direction="column"
-              alignItems="center"
-              justify="center"
-            >
-              {state.map((ingredient, index) => <Ingredient ingredient={ingredient} index={index} />)}
-              {provided.placeholder}
-            </Grid >
-          )}
-        </Droppable>
-      </DragDropContext>
+      <Box mx={20}>
+        <Paper style={{ backgroundColor: "white" }} elevation={3}>
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="recipe">
+              {provided => (
+                <Grid
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  container
+                  direction="column"
+                  alignItems="center"
+                  justify="center"
+                >
+                  {state.map((ingredient, index) => <Ingredient ingredient={ingredient} index={index} />)}
+                  {provided.placeholder}
+                </Grid >
+              )}
+            </Droppable>
+          </DragDropContext>
+        </Paper>
+      </Box>
     )
   );
 }
