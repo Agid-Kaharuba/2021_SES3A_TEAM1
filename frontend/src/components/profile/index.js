1831c0a9c516f46ad4d4ae81514ff15e895e5d64
Anna Tian <annatian1997@gmail.com>
2021-08-14 20:52:52 +1000

    re-develop profile layout

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button, TextField, Container, Typography } from "@material-ui/core";
+import { Button, TextField, Container, Typography, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import BackButton from "../backbutton";
+import EditIcon from '@material-ui/icons/Edit';
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -19,22 +20,39 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(0.5)
   },
+  profileImg: {
+    position: "relative",
+    alignSelf: "start"
+  },
   image: {
     height: 200,
     width: 200,
     borderRadius: 100,
     margin: theme.spacing(2),
   },
+  uploadBtn: {
+    position: "absolute",
+    bottom: "0px",
+    right: "0px"
+  },
   imageUpload: {
     display: "none",
   },
   container: {
-    alignItems: "center",
-    display: 'flex',
-    flexDirection: 'column',
+    display: "grid",
+    gridTemplateColumns: "1fr 4fr",
+    columnGap: "2vw"
   },
-  uploadBtn: {
-    marginLeft: theme.spacing(4),
+  name: {
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    columnGap: "2vw",
+  },
+  labelField: {
+    marginTop: theme.spacing(3),
+  },
+  saveBtn: {
+    textAlign: "center"
   }
 }));
 
@@ -45,17 +63,29 @@ function ProfilePicture(props) {
   // Just propping them in for now.
   const { imageUploader, handleImageUpload } = props;
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleDialogOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+  };
+
 
   return (
-    <>
+    <div>
       {/*Accepting only files with image type*/}
       <input type="file" id="input" accept="image/*" onChange={handleImageUpload} ref={imageUploader} className={classes.imageUpload} />
-      <div className={classes.container}>
+      <div className={classes.profileImg}>
         <img className={classes.image} src={props.image.data} alt="profile image" />
-        <br />
-        <Button size="small" onClick={() => imageUploader.current.click()} variant="outlined" color="secondary" className={classes.uploadBtn}>Upload Image</Button>
+        <IconButton onClick={() => imageUploader.current.click()} className={classes.uploadBtn}><EditIcon fontSize="large" /></IconButton>
       </div>
-    </>
+      <div>
+        <Button onClick={handleDialogOpen} variant="outlined" color="secondary" className={classes.changePsw}>Change Password</Button>
+      </div>
+    </div>
   )
 }
 
@@ -65,93 +95,68 @@ export default function Profile(props) {
 
   return (
     <Container maxWidth="md">
+      <Typography variant="h4" className={classes.heading}>My Profile</Typography>
       <div className={classes.container}>
-        <Typography variant="h4" className={classes.heading}>PROFILE</Typography>
-        {/* TODO: implement profile picture */}
-        {<ProfilePicture {...props} />}
-
+        <ProfilePicture {...props} />
+        <form onSubmit={saveChanges}>
+          <div className={classes.name}>
+            <div className={classes.labelField}>
+              <Typography variant="subtitle1" className={classes.typography}>First Name</Typography>
+              <TextField
+                id="outlined-required"
+                name="firstname"
+                onChange={handleChange}
+                value={employee.firstname}
+                variant="outlined"
+                fullWidth
+                className={classes.textField}
+              // label="First Name"
+              />
+            </div>
+            <div className={classes.labelField}>
+              <Typography variant="subtitle1" className={classes.typography}>Last Name</Typography>
+              <TextField
+                id="outlined-required"
+                name="lastname"
+                onChange={handleChange}
+                value={employee.lastname}
+                variant="outlined"
+                fullWidth
+                className={classes.textField}
+              />
+            </div>
+          </div>
+          <div className={classes.labelField}>
+            <Typography variant="subtitle1" className={classes.typography}>Staff ID</Typography>
+            <TextField
+              id="outlined-required"
+              name="staffid"
+              onChange={handleChange}
+              value={employee.staffid}
+              variant="outlined"
+              fullWidth
+              className={classes.textField}
+            />
+          </div>
+          <div className={classes.labelField}>
+            <Typography variant="subtitle1" className={classes.typography}>Email</Typography>
+            <TextField
+              id="outlined-required"
+              name="email"
+              onChange={handleChange}
+              value={employee.email}
+              variant="outlined"
+              fullWidth
+              className={classes.textField}
+            />
+          </div>
+        </form>
       </div>
-
-      <form onSubmit={saveChanges}>
-        <br />
-        <Typography variant="subtitle1" className={classes.typography}>First Name</Typography>
-        <TextField
-          id="outlined-required"
-          name="firstname"
-          onChange={handleChange}
-          value={employee.firstname}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        // label="First Name"
-        />
-        <br />
-        <Typography variant="subtitle1" className={classes.typography}>Last Name</Typography>
-        <TextField
-          id="outlined-required"
-          name="lastname"
-          onChange={handleChange}
-          value={employee.lastname}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        />
-        <br />
-        <Typography variant="subtitle1" className={classes.typography}>Email</Typography>
-        <TextField
-          id="outlined-required"
-          name="email"
-          onChange={handleChange}
-          value={employee.email}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        />
-        <br />
-        <Typography variant="subtitle1" className={classes.typography}>Staff ID</Typography>
-        <TextField
-          id="outlined-required"
-          name="staffid"
-          onChange={handleChange}
-          value={employee.staffid}
-          variant="outlined"
-          fullWidth
-          className={classes.textField}
-        />
-        <br />
-        <Typography variant="subtitle1" className={classes.typography}>New Password</Typography>
-        <TextField
-          id="outlined-password-input"
-          type="password"
-          name="newPassword"
-          autoComplete="current-password"
-          onChange={handleChange}
-          value={employee.newPassword}
-          variant="outlined"
-          fullWidth
-          className={classes.password}
-        />
-        <br />
-        <Typography variant="subtitle1" className={classes.typography}>Current Password*</Typography>
-        <TextField
-          id="outlined-password-input"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          onChange={handleChange}
-          value={employee.password}
-          variant="outlined"
-          fullWidth
-          required
-          className={classes.password}
-        />
-        <br />
-        <div className={classes.container}>
-          <Button variant="contained" color="secondary" className={classes.button} type="submit">
-            Save
-          </Button>
-        </div>
-      </form>
+      <div className={classes.saveBtn}>
+        <Button variant="contained" color="secondary" className={classes.button} type="submit">
+          Save
+        </Button>
+      </div>
     </Container>
   )
 }
