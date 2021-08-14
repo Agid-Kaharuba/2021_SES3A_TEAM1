d2b94b96b6a2835be6aee15470f673df8239bbb8
Anna Tian <annatian1997@gmail.com>
2021-08-15 00:13:46 +1000

    add change password popup modal

--- frontend/src/components/profile/index.js
+++ frontend/src/components/profile/index.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { Button, TextField, Container, Typography, IconButton } from "@material-ui/core";
+import { Button, TextField, Container, Typography, IconButton, Dialog } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import BackButton from "../backbutton";
 import EditIcon from '@material-ui/icons/Edit';
+import CloseIcon from '@material-ui/icons/Close';
 
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1)
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  changePswForm: {
+    padding: "16px 24px",
+
   },
   textField: {
     marginTop: theme.spacing(0.5),
@@ -56,6 +68,86 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+
+function DialogTitle(props) {
+  const classes = useStyles();
+  const { children, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+}
+
+function ChangePasswordDialog(props) {
+  const classes = useStyles();
+  const { onClose, open, employee, handleChange, saveChanges } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open} disableBackdropClick disableEscapeKeyDown fullWidth="true" maxWidth="sm">
+      <DialogTitle onClose={handleClose}>Change Password</DialogTitle>
+      <form onSubmit={saveChanges} className={classes.changePswForm}>
+        <Typography variant="subtitle1" className={classes.typography}>Current Password*</Typography>
+        <TextField
+          id="outlined-password-input"
+          type="password"
+          name="password"
+          autoComplete="current-password"
+          onChange={handleChange}
+          value={employee.password}
+          variant="outlined"
+          required
+          fullWidth
+          className={classes.password}
+        />
+        <Typography variant="subtitle1" className={classes.typography}>{employee.password}</Typography>
+        <Typography variant="subtitle1" className={classes.typography}>New Password*</Typography>
+        <TextField
+          id="outlined-password-input"
+          type="password"
+          name="newPassword"
+          autoComplete="current-password"
+          onChange={handleChange}
+          value={employee.newPassword}
+          variant="outlined"
+          fullWidth
+          required
+          className={classes.password}
+        />
+        <Typography variant="subtitle1" className={classes.typography}>Confirm Password*</Typography>
+        <TextField
+          id="outlined-password-input"
+          type="password"
+          name="confirmPassword"
+          autoComplete="current-password"
+          onChange={handleChange}
+          value={employee.confirmPassword}
+          variant="outlined"
+          fullWidth
+          required
+          error={(employee.confirmPassword !== undefined) && (employee.newPassword !== employee.confirmPassword)}
+          helperText={(employee.confirmPassword !== undefined) && (employee.newPassword !== employee.confirmPassword) ? 'Password do not match' : ''}
+          className={classes.password}
+        />
+        <div className={classes.saveBtn}>
+          <Button variant="contained" color="secondary" className={classes.button} type="submit">
+            Save
+          </Button>
+        </div>
+      </form>
+    </Dialog>
+  );
+}
+
 function ProfilePicture(props) {
   const classes = useStyles();
   // TODO: I dont know what these are for
@@ -84,6 +176,7 @@ function ProfilePicture(props) {
       </div>
       <div>
         <Button onClick={handleDialogOpen} variant="outlined" color="secondary" className={classes.changePsw}>Change Password</Button>
+        <ChangePasswordDialog open={open} onClose={handleClose} {...props}></ChangePasswordDialog>
       </div>
     </div>
   )
@@ -98,7 +191,7 @@ export default function Profile(props) {
       <Typography variant="h4" className={classes.heading}>My Profile</Typography>
       <div className={classes.container}>
         <ProfilePicture {...props} />
-        <form onSubmit={saveChanges}>
+        <form>
           <div className={classes.name}>
             <div className={classes.labelField}>
               <Typography variant="subtitle1" className={classes.typography}>First Name</Typography>
@@ -153,7 +246,7 @@ export default function Profile(props) {
         </form>
       </div>
       <div className={classes.saveBtn}>
-        <Button variant="contained" color="secondary" className={classes.button} type="submit">
+        <Button variant="contained" color="secondary" className={classes.button} type="submit" onClick={saveChanges}>
           Save
         </Button>
       </div>
