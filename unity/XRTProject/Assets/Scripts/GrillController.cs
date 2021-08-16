980c432d75a1c5bce14ad91ba8351eca327bb143
MatthewDBarrett <12948612@student.uts.edu.au>
2021-08-16 16:12:56 +1000

    Steam added to grill
    Using VFX Graph, a heat and steam effect has been added when the grill is turned on

--- unity/XRTProject/Assets/Scripts/GrillController.cs
+++ unity/XRTProject/Assets/Scripts/GrillController.cs
@@ -6,10 +6,12 @@ public class GrillController : MonoBehaviour {
 
     Animator anim;
     Renderer rend;
+    GameObject steam;
 
     void Start() {
         anim = gameObject.GetComponent<Animator> ();
         rend = gameObject.transform.Find("grill").GetComponent<Renderer> ();
+        steam = gameObject.transform.Find ( "Steam" ).gameObject;
     }
 
     void Update() {
@@ -23,18 +25,18 @@ public class GrillController : MonoBehaviour {
             anim.SetBool ( "grillOn", true );
         }
 
-        StartCoroutine ( ToggleLight() );
+        StartCoroutine ( TogglePower() );
     }
 
-    IEnumerator ToggleLight () {
+    IEnumerator TogglePower () {
         yield return new WaitForSeconds ( 0.5f );
 
         if ( anim.GetBool ( "grillOn" ) ) {
             rend.material.SetColor ( "_EmissiveColor", Color.green * 1000f );
-            //rend.material.SetFloat ( "_EmissiveIntensity", 100f );
+            steam.SetActive ( true );
         } else {
             rend.material.SetColor ( "_EmissiveColor", Color.red * 1000f );
-            //rend.material.SetFloat ( "_EmissiveIntensity", 100f );
+            steam.SetActive ( false );
         }
 
         yield return null;
