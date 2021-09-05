49025ab03f5b33c6fc81b5668babc9ceef98aad3
13204767 <13204767@student.uts.edu.au>
2021-09-05 12:56:23 +1000

    Created API file for floor.js on the frontend

---
+++ frontend/src/helpers/api/floor.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { API_HOST } from ".";
+
+export const getAllFloors = async (token) => {
+	const res = await axios.get(`${API_HOST}/floor`,
+		{
+			headers: { Authorization: `Bearer ${token}` },
+		}
+	);
+	return res;
+}
+
+export const createFloor = async (token, data) => {
+	const res = await axios.post(`${API_HOST}/floor`,
+		data,
+		{
+			headers: { Authorization: `Bearer ${token}` },
+		}
+	);
+	return res;
+}
+
+export const getFloor = async (token, floorId) => {
+	const res = await axios.get(`${API_HOST}/floor/${floorId}`,
+		{
+			headers: { Authorization: `Bearer ${token}` },
+		}
+	);
+	return res;
+}
+
+export const updateFloor = async (token, floorId, data) => {
+	const res = await axios.put(`${API_HOST}/floor/${floorId}`,
+		data,
+		{
+			headers: { Authorization: `Bearer ${token}` },
+		}
+	);
+	return res;
+}
+
+export const deleteFloor = async (token, userId) => {
+	const res = await axios.delete(`${API_HOST}/floor/${floorId}`,
+		{
+			headers: { Authorization: `Bearer ${token}` },
+		}
+	);
+	return res;
+}
