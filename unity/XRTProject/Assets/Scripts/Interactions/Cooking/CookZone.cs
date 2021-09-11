03bec7ef483ac59c5ddbf4fb4d85f6ebe1715fa3
Agid-Kaharuba <razinagida@gmail.com>
2021-09-11 10:35:13 +1000

    Adjust grill collider and have on and off state

--- unity/XRTProject/Assets/Scripts/Interactions/Cooking/CookZone.cs
+++ unity/XRTProject/Assets/Scripts/Interactions/Cooking/CookZone.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class CookZone : MonoBehaviour
 {
     [SerializeField] private bool oneSidedCooking;
-    
-    public float strength = 1f;
+    public bool isActive;
+    [Min(0)] public float strength = 1f;
     
     private void OnTriggerStay(Collider other)
     {
+        if (!isActive) return;
+        
         Cookable cookable = other.GetComponent<Cookable>();
 
         if (cookable != null)
         {
             cookable.TickCooking(strength, Time.fixedDeltaTime, oneSidedCooking);
         }
+    }
+
+    public void SetCanCook(bool isActive)
+    {
+        this.isActive = isActive;
+    }
+    
+    public void SetStrength(float strength)
+    {
+        this.strength = strength;
     }
 
     private void OnValidate()
