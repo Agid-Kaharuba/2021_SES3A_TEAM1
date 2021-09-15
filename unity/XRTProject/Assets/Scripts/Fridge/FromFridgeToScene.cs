02876d19bafa8ef97659ab26c7825bc04a9aaadd
MatthewDBarrett <12948612@student.uts.edu.au>
2021-09-15 20:13:52 +1000

    Ingredient Detection
    The From Fridge to Scene script has been changed to record the items in the fridge at the start of the program and record as the items are removed and added back to the fridge.

--- unity/XRTProject/Assets/Scripts/Fridge/FromFridgeToScene.cs
+++ unity/XRTProject/Assets/Scripts/Fridge/FromFridgeToScene.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FromFridgeToScene : MonoBehaviour
-{
+public class FromFridgeToScene : MonoBehaviour {
 
-    
-    void OnCollisionExit(Collision collision)
-    {
+    private List<GameObject> startItems = new List<GameObject> ();
+    private List<GameObject> removedItems = new List<GameObject> ();
 
-        if (collision.gameObject.name == "MainFridge")
-        {
-            gameObject.transform.parent = GameObject.Find("ItemsFromTheFridge").transform;
-        }
-        
+    private GameObject itemsInFridge;
+
+    private void Start () {
+        RegisterInitialItems ();
     }
+
+    private void OnTriggerExit ( Collider other ) {
+        if ( other.transform.tag == "Ingredient" ) {
+            Debug.Log ( "EXIT: " + other.gameObject.name );
+            removedItems.Add ( other.gameObject );
+            startItems.Remove ( other.gameObject );
+        }
+    }
+
+    private void OnTriggerEnter ( Collider other ) {
+        if ( other.transform.tag == "Ingredient" ) {
+            Debug.Log ( "ENTER: " + other.gameObject.name );
+            removedItems.Remove ( other.gameObject );
+            startItems.Add ( other.gameObject );
+        }
+    }
+
+    private void RegisterInitialItems () {
+        itemsInFridge = GameObject.Find ( "ItemsInTheFridge" );
+        for ( int i = 0; i < itemsInFridge.transform.childCount; i++ ) {
+            GameObject childA = itemsInFridge.transform.GetChild ( i ).gameObject;
+            for ( int j = 0; j < childA.transform.childCount; j++ ) {
+                GameObject childB = childA.transform.GetChild( j ).gameObject;
+                startItems.Add ( childB );
+            }
+        }
+    }
+
 }
