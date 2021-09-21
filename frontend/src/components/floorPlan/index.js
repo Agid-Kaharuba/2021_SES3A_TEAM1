60c561c33334283a40647309b3350b43a7851420
KelvinUTS <49703204+KelvinUTS@users.noreply.github.com>
2021-09-21 14:10:25 +1000

    Fixing Draggable Kitchen Items
    fixing in progress

--- frontend/src/components/floorPlan/index.js
+++ frontend/src/components/floorPlan/index.js
@@ -4,7 +4,10 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Button, Typography, Divider, } from "@material-ui/core";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import styled from "styled-components";
+
+import { AuthContext } from "../../context/auth";
 
 import KitchenItem from './kitchenItem';
 
