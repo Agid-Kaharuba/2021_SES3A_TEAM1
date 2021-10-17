0703968a815e835b03bb3a46cc0941aefbef4613
13204767 <13204767@student.uts.edu.au>
2021-10-17 23:17:39 +1100

    Added padding to the bottom for recipe page

--- frontend/src/pages/recipe/recipesList/recipesList.js
+++ frontend/src/pages/recipe/recipesList/recipesList.js
@@ -50,6 +50,7 @@ const useStyles = makeStyles({
   gridContainer: {
     paddingLeft: "20px",
     paddingRight: "20px",
+    paddingBottom: "50px",
   },
   trainingModules: {
     display: "grid",
