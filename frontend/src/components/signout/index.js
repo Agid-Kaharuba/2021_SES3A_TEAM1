9548aacb7b21a186b66a7eed0d4635847f0ccb23
mitcmurph <mitcmurph@gmail.com>
2021-10-25 03:22:44 -0700

    delete files for tacotron deployment

--- frontend/src/components/signout/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
-import { AuthContext } from "../../context/auth";
-
-export default function SignOut(props) {
-    const { authState, setAuthState } = React.useContext(AuthContext);
-    setAuthState({authenticated: false});
-    return <Redirect to="/" />;
-}
--- frontend/src/helpers/appTheme/index.js
+++
@@ -1,8 +0,0 @@
-import { createMuiTheme } from "@material-ui/core";
-
-const appTheme = createMuiTheme({
-  // Add styling here e.g appGreen: "#00ff00", then reference it like theme.appGreen
-  mainColor: "#4F7CE0"
-});
-
-export default appTheme;
