73e7edfc1a836949c884a4141a9bdd3edc00ad42
mitcmurph <mitcmurph@gmail.com>
2021-10-27 06:39:54 -0700

    proxy requests through the backend

--- backend/src/routes/inference-router.ts
+++ backend/src/routes/inference-router.ts
@@ -4,6 +4,28 @@ import InferenceController from '../controllers/inference';
 export const InferenceRoute = (app: Express, controller: InferenceController) => {
   /**
      * @swagger
+     * /inferenceNode:
+     *  post:
+     *   description: Create a text-to-speech file
+     *   tags: [inference]
+     *   requestBody:
+     *    required: true,
+     *    content:
+     *     application/json:
+     *      schema:
+     *       type: object
+     *       properties:
+     *        text:
+     *         type: string
+     *         example: This is going to say something!
+     *   responses:
+     *    200:
+     *     description: Success
+     */
+  app.post('/inferenceWithNode', controller.getInference);
+
+  /**
+     * @swagger
      * /inference:
      *  post:
      *   description: Create a text-to-speech file
@@ -22,5 +44,5 @@ export const InferenceRoute = (app: Express, controller: InferenceController) =>
      *    200:
      *     description: Success
      */
-  app.post('/inference', controller.getInference);
+  app.post('/inference', controller.proxy);
 }
