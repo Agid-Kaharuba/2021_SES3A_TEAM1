cdf9b8ad96691e03adbc1c50c0c4afe6b5db69c5
13204767 <13204767@student.uts.edu.au>
2021-10-31 23:04:21 +1100

    Removed useless text

--- frontend/src/pages/aboutus/aboutus.js
+++ frontend/src/pages/aboutus/aboutus.js
@@ -2,17 +2,28 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 // IMPORT COMPONENTS
-import { Box, Grid, Container, Typography, Paper, Avatar, Card, CardActionArea, CardContent, CardMedia, Button, CardActions } from "@material-ui/core";
+import {
+  Box,
+  Grid,
+  Container,
+  Typography,
+  Paper,
+  Avatar,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  CardActions,
+} from "@material-ui/core";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 
 // IMPORT IMAGES
 
-
-import ProjectOutlineImage from '../../images/projuout.jpg';
-import TeamCommunicationImage from '../../images/teamcom.jpg';
-import VrImage from '../../images/vrthingy.jpg';
-import TeamImage from '../../images/teams.jpg';
-
+import ProjectOutlineImage from "../../images/projuout.jpg";
+import TeamCommunicationImage from "../../images/teamcom.jpg";
+import VrImage from "../../images/vrthingy.jpg";
+import TeamImage from "../../images/teams.jpg";
 
 // import { Link } from "react-router-dom";
 // import { AuthContext } from "../../context/auth";
@@ -20,7 +31,7 @@ import TeamImage from '../../images/teams.jpg';
 const WhiteTextTypography = withStyles({
   root: {
     color: "#FFFFFF",
-  }
+  },
 })(Typography);
 
 const profileStyles = makeStyles((theme) => ({
@@ -36,14 +47,14 @@ const useStyles = makeStyles({
   },
   root: {
     maxWidth: 300,
-    height: 300
+    height: 300,
   },
   margins: {
     gridGap: 10,
   },
-  heading:{
-    color:  "#FFFFFF",
-  }
+  heading: {
+    color: "#FFFFFF",
+  },
 });
 
 export default function AboutUsPage() {
@@ -52,31 +63,52 @@ export default function AboutUsPage() {
   const profiles = profileStyles();
   const classes = useStyles();
   //   if (authState.authenticated) {
-  //     return authState.user.claims.teacher ? <Redirect to="/teacher/subjectList" />: <Redirect to="/student/dashboard" />; 
+  //     return authState.user.claims.teacher ? <Redirect to="/teacher/subjectList" />: <Redirect to="/student/dashboard" />;
   //   } else {
   return (
     // Orignial Gradient linear-gradient(45deg, #662D8C  10%, #ED1E79)
     <Grid
       container
       spacing={2}
-      direction='column'
-      alignItems='center'
-      justify='center'
-      style={{ minHeight: '100vh', backgroundImage: "linear-gradient(45deg, #662D8C  10%, #ED1E79)" }}>
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{
+        minHeight: "100vh",
+        backgroundImage: "linear-gradient(45deg, #662D8C  10%, #ED1E79)",
+      }}
+    >
       <Grid item>
         <br></br>
-        <WhiteTextTypography variant='h3'>
-          <Box style={{ fontWeight: '600', letterSpacing: 6 }} display='inline'> About Us</Box>
+        <WhiteTextTypography variant="h3">
+          <Box style={{ fontWeight: "600", letterSpacing: 6 }} display="inline">
+            {" "}
+            About Us
+          </Box>
         </WhiteTextTypography>
       </Grid>
-      <Grid container direction="column" alignItems='flex-start'>
+      <Grid container direction="column" alignItems="flex-start">
         <Container>
-          <h2 className={classes.heading}>Our Storyy</h2>
-          <WhiteTextTypography>What is Lorem Ipsum?
-          Our Team came up with the revolutionary idea that would transform the training sector completely with the help of Virtual Reality also the Technology of Future. Our team earlier this year came up with a great solution on how to train users in different industries. Our technology would allow users to look around themselves, walk up to close computer-generated objects, and interact with different items from the comfort of their home.
+          <h2 className={classes.heading}>Our Story</h2>
+          <WhiteTextTypography>
+            Our Team came up with the revolutionary idea that would transform
+            the training sector completely with the help of Virtual Reality also
+            the Technology of Future. Our team earlier this year came up with a
+            great solution on how to train users in different industries. Our
+            technology would allow users to look around themselves, walk up to
+            close computer-generated objects, and interact with different items
+            from the comfort of their home.
           </WhiteTextTypography>
-          <Grid container direction="row" alignItems='flex-start' className={classes.margins} >
-            <Card className={classes.root} onClick={() => history.push('/aboutus/projectoutline')}>
+          <Grid
+            container
+            direction="row"
+            alignItems="flex-start"
+            className={classes.margins}
+          >
+            <Card
+              className={classes.root}
+              onClick={() => history.push("/aboutus/projectoutline")}
+            >
               <CardActionArea>
                 <CardMedia
                   className={classes.media}
@@ -86,13 +118,20 @@ export default function AboutUsPage() {
                   <Typography gutterBottom variant="h5" component="h2">
                     Project Outline
                   </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
                     The aim of the project is to create training in VR.
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
-            <Card className={classes.root} onClick={() => history.push('/aboutus/teamcommunication')}>
+            <Card
+              className={classes.root}
+              onClick={() => history.push("/aboutus/teamcommunication")}
+            >
               <CardActionArea>
                 <CardMedia
                   className={classes.media}
@@ -102,40 +141,53 @@ export default function AboutUsPage() {
                   <Typography gutterBottom variant="h5" component="h2">
                     Team Communication
                   </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
                     Information of how our team communicate between each other.
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
-            <Card className={classes.root} onClick={() => history.push('/aboutus/vrexperience')}>
+            <Card
+              className={classes.root}
+              onClick={() => history.push("/aboutus/vrexperience")}
+            >
               <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={VrImage}
-                />
+                <CardMedia className={classes.media} image={VrImage} />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="h2">
                     VR experiences
                   </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
                     Some experiences of clients using the our application.
                   </Typography>
                 </CardContent>
               </CardActionArea>
             </Card>
-            <Card className={classes.root} onClick={() => history.push('/aboutus/theteam')}>
+            <Card
+              className={classes.root}
+              onClick={() => history.push("/aboutus/theteam")}
+            >
               <CardActionArea>
-                <CardMedia
-                  className={classes.media}
-                  image={TeamImage}
-                />
+                <CardMedia className={classes.media} image={TeamImage} />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="h2">
                     The Team
                   </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                    Our team consist of three division: Unity team, Backend team & Frontend team.
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    Our team consist of three division: Unity team, Backend team
+                    & Frontend team.
                   </Typography>
                 </CardContent>
               </CardActionArea>
@@ -144,7 +196,6 @@ export default function AboutUsPage() {
         </Container>
       </Grid>
     </Grid>
-
-  )
+  );
   //   }
 }
